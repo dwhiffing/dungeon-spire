56bdae3e5f89c796ca254f4f6fc1e417a34b3f12
Daniel Whiffing <danielwhiffing@gmail.com>
2022-08-14 15:51:13 -0400

    Allows mute in menu

--- src/scenes/Menu.ts
+++ src/scenes/Menu.ts
@@ -7,12 +7,19 @@ export default class extends Phaser.Scene {
     const { width, height } = this.cameras.main
     this.sound.stopAll()
     this.sound.play('menu-music', { loop: true, volume: 0.5 })
+    let started = false
     const start = () => {
+      if (started) return
+      started = true
       this.cameras.main.fadeOut(1000, 0, 0, 0)
       this.time.delayedCall(1000, () => {
         this.scene.start('Game', { level: 1 })
       })
     }
+
+    this.input.keyboard.addKey('M').addListener('down', () => {
+      this.sound.mute = !this.sound.mute
+    })
 
     this.input.keyboard.addKey('SPACE').addListener('down', start)
 
